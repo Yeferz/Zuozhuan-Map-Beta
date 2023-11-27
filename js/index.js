f88d6dcd2583c0a3206156d5f2231221d1e3cb08
Yeferz <naiyazao@gmail.com>
2023-11-27 14:57:46 +0800

    Name, Polity and Hanzi function complete

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 /** @format */
 
+let searchBy;
 let locales;
 let filterCondition = /[a-z]/;
 var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -15,7 +16,24 @@ var Esri_WorldStreetMap = L.tileLayer(
 			'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012',
 	}
 );
-
+// const reignYears = {
+// 	Ai: [494, 467],
+// 	Ding: [508, 494],
+// 	Zhao: [540, 508],
+// 	Xiang: [571, 540],
+// 	Cheng: [589, 571],
+// 	Xuan: [607, 589],
+// 	Wen: [625, 607],
+// 	Xi: [658, 625],
+// 	Min: [660, 658],
+// 	Zhuang: [692, 660],
+// 	Huan: [710, 692],
+// 	Yin: [722, 710],
+// };
+// for (let i = 0; i < reignYears.length; i++) {
+// 	year = reignYears[i];
+// 	console.log(year);
+// }
 var map = L.map('map', {
 	center: [34.669724, 112.442223],
 	zoom: 10,
@@ -32,6 +50,56 @@ var baseMaps = {
 // 	Chu: Chu,
 // };
 var layerControl = L.control.layers(baseMaps).addTo(map);
+
+fetch('js/data.json')
+	.then((response) => {
+		if (!response.ok) {
+			throw new Error('Network response was not ok');
+		}
+		return response.json();
+	})
+	.then((data) => {
+		// Process the retrieved JSON data
+		// console.log(data);
+		locales = data;
+		// console.log(locales);
+		// Use the retrieved data for further operations (e.g., creating markers on a map)
+		sortIndividualLocalesOnLoad = function (data) {
+			for (let i = 0; i < data.length; i++) {
+				let indexedLocale = data[i];
+				// console.log(indexedLocale);
+				// console.log(indexedLocale.latitude);
+				if (
+					indexedLocale.latitude < 42 &&
+					indexedLocale.latitude > 27 &&
+					indexedLocale.longitude < 123 &&
+					indexedLocale.longitude > 99
+				) {
+					//Here we can create a condition to skip ahead to the next thing if the entry is filtered out.
+					marker = L.marker([
+						indexedLocale.latitude,
+						indexedLocale.longitude,
+					]).addTo(markerGroup);
+					let indexedLocaleString = JSON.stringify(
+						indexedLocale.hanzi +
+							'</p><p>' +
+							indexedLocale.name +
+							'</p><p>' +
+							indexedLocale.polity +
+							'</p><p>' +
+							indexedLocale.entries
+					);
+					indexedLocaleString = indexedLocaleString.slice(1, -1);
+					// console.log(indexedLocaleString);
+					marker.bindPopup(indexedLocaleString).openPopup();
+				} else {
+					// console.log('filter this one out');
+				}
+			}
+		};
+		sortIndividualLocalesOnLoad(locales);
+	});
+
 retrieveData = function () {
 	fetch('js/data.json')
 		.then((response) => {
@@ -98,19 +166,32 @@ retrieveData = function () {
 };
 
 let inputResult = document.getElementById('searchInput');
-var searchBy = document.getElementById('searchBy').value;
+let clearResult = document.getElementById('Clear');
 console.log(searchBy);
 console.log(inputResult);
 var searchPolity = document.getElementById('Search');
 function setPolity() {
 	console.log('Polity filter applied');
+	searchBy = document.getElementById('searchBy').value;
 	var searchValue = inputResult.value;
 	filterCondition = RegExp(searchValue);
 	console.log(filterCondition);
 	markerGroup.clearLayers();
 	retrieveData();
 }
+function clearPolity() {
+	console.log('Polity cleared');
+	var searchValue = /[a-z]/;
+	filterCondition = RegExp(searchValue);
+	console.log(filterCondition);
+	searchBy = 'name';
+	searchInput.value = '';
+	markerGroup.clearLayers();
+	retrieveData();
+}
 searchPolity.addEventListener('click', setPolity);
+clearResult.addEventListener('click', clearPolity);
+retrieveData();
 
 // Function to remove all markers from the map
 // function removeAllMarkers() {
@@ -219,4 +300,3 @@ searchPolity.addEventListener('click', setPolity);
 // 		console.log('Database connection closed.');
 // 	}
 // });
-retrieveData();
