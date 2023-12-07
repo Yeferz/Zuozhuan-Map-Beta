9810b2645e0304a5cd8cc878c8c8119385e00169
Yeferz <naiyazao@gmail.com>
2023-12-07 18:04:17 +0800

    fixed

--- js/index.js
+++ js/index.js
@@ -3,178 +3,157 @@
 let searchBy;
 let locales;
 let filterCondition = /[a-z]/;
+let rangeHigh = 722;
+let rangeLow = 480;
 //This is the slider, taken from here https://digital-geography.com/filter-leaflet-maps-slider/
-		var slidervar = document.getElementById('slider');
-		noUiSlider.create(slidervar, {
-			connect: true,
-			start: [480, 722],
-			range: {
-				min: 480,
-				max: 722,
-			},
-			tooltips: [true, true], // Show tooltips for both handles
-			direction: 'rtl',
-		});
+var slidervar = document.getElementById('slider');
+noUiSlider.create(slidervar, {
+	connect: true,
+	start: [480, 722],
+	range: {
+		min: 480,
+		max: 722,
+	},
+	tooltips: [true, true], // Show tooltips for both handles
+	direction: 'rtl',
+});
 
-		let inputNumberMin = document.getElementById('input-number-min');
-		let inputNumberMax = document.getElementById('input-number-max');
-		let rangeHigh;
-		let rangeLow;
-		inputNumberMin.addEventListener('change', function () {
-			slidervar.noUiSlider.set([this.value, null]);
-		});
-		inputNumberMax.addEventListener('change', function () {
-			slidervar.noUiSlider.set([null, this.value]);
-		});
+let inputNumberMin = document.getElementById('input-number-min');
+let inputNumberMax = document.getElementById('input-number-max');
+inputNumberMin.addEventListener('change', function () {
+	slidervar.noUiSlider.set([this.value, null]);
+});
+inputNumberMax.addEventListener('change', function () {
+	slidervar.noUiSlider.set([null, this.value]);
+});
 
-		var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-			maxZoom: 19,
-			attribution: '© OpenStreetMap',
-		});
+var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '© OpenStreetMap',
+});
 
-		var marker;
-		var Esri_WorldTerrain = L.tileLayer(
-			'https://server.arcgisonline.com/ArcGIS/rest/services/World_Terrain_Base/MapServer/tile/{z}/{y}/{x}',
-			{
-				attribution:
-					'Tiles &copy; Esri &mdash; Source: USGS, Esri, TANA, DeLorme, and NPS',
-				maxZoom: 13,
-			}
-		);
-		var Esri_WorldPhysical = L.tileLayer(
-			'https://server.arcgisonline.com/ArcGIS/rest/services/World_Physical_Map/MapServer/tile/{z}/{y}/{x}',
-			{
-				attribution:
-					'Tiles &copy; Esri &mdash; Source: US National Park Service',
-				maxZoom: 8,
-			}
-		);
-		var USGS_USImagery = L.tileLayer(
-			'https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}',
-			{
-				maxZoom: 20,
-				attribution:
-					'Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>',
-			}
-		);
-		var Esri_WorldStreetMap = L.tileLayer(
-			'https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}',
-			{
-				attribution:
-					'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012',
-			}
-		);
-		//This is something I hope to use later to add a year filter so that the user can search by date. At the moment I haven't developed this part further since it's quite complicated to implement
+var marker;
+var Esri_WorldTerrain = L.tileLayer(
+	'https://server.arcgisonline.com/ArcGIS/rest/services/World_Terrain_Base/MapServer/tile/{z}/{y}/{x}',
+	{
+		attribution:
+			'Tiles &copy; Esri &mdash; Source: USGS, Esri, TANA, DeLorme, and NPS',
+		maxZoom: 13,
+	}
+);
+var Esri_WorldPhysical = L.tileLayer(
+	'https://server.arcgisonline.com/ArcGIS/rest/services/World_Physical_Map/MapServer/tile/{z}/{y}/{x}',
+	{
+		attribution: 'Tiles &copy; Esri &mdash; Source: US National Park Service',
+		maxZoom: 8,
+	}
+);
+var USGS_USImagery = L.tileLayer(
+	'https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryOnly/MapServer/tile/{z}/{y}/{x}',
+	{
+		maxZoom: 20,
+		attribution:
+			'Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>',
+	}
+);
+var Esri_WorldStreetMap = L.tileLayer(
+	'https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}',
+	{
+		attribution:
+			'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012',
+	}
+);
+var map = L.map('map', {
+	center: [34.669724, 112.442223],
+	zoom: 10,
+	layers: [Esri_WorldPhysical],
+});
+var markerGroup = L.layerGroup().addTo(map);
 
-		// const reignYears = {
-		// 	Ai: [494, 467],
-		// 	Ding: [508, 494],
-		// 	Zhao: [540, 508],
-		// 	Xiang: [571, 540],
-		// 	Cheng: [589, 571],
-		// 	Xuan: [607, 589],
-		// 	Wen: [625, 607],
-		// 	Xi: [658, 625],
-		// 	Min: [660, 658],
-		// 	Zhuang: [692, 660],
-		// 	Huan: [710, 692],
-		// 	Yin: [722, 710],
-		// };
-		// for (let i = 0; i < reignYears.length; i++) {
-		// 	year = reignYears[i];
-		// 	console.log(year);
-		// }
-		var map = L.map('map', {
-			center: [34.669724, 112.442223],
-			zoom: 10,
-			layers: [Esri_WorldPhysical],
-		});
-		var markerGroup = L.layerGroup().addTo(map);
+var baseMaps = {
+	ESRI_Physical: Esri_WorldPhysical,
+	ESRI_Terrain: Esri_WorldTerrain,
+	OpenStreetMap: osm,
+	ESRI_WorldStreetMap: Esri_WorldStreetMap,
+	USGS: USGS_USImagery,
+};
 
-		var baseMaps = {
-			ESRI_Physical: Esri_WorldPhysical,
-			ESRI_Terrain: Esri_WorldTerrain,
-			OpenStreetMap: osm,
-			ESRI_WorldStreetMap: Esri_WorldStreetMap,
-			USGS: USGS_USImagery,
-		};
+var layerControl = L.control.layers(baseMaps).addTo(map);
+let outScopeSlider;
+//This is the bit which initially collects the data from data.json and turns it into markers on the leaflet map, this defines what you see when you first open the main page.
+fetch('js/data.json')
+	.then((response) => {
+		if (!response.ok) {
+			throw new Error('Network response was not ok');
+		}
+		return response.json();
+	})
+	.then((data) => {
+		// Process the retrieved JSON data
+		// console.log(data);
+		locales = data;
+		// console.log(locales);
+		// Use the retrieved data for further operations (e.g., creating markers on a map)
+		sortIndividualLocalesOnLoad = function (data) {
+			for (let i = 0; i < data.length; i++) {
+				let indexedLocale = data[i];
+				// console.log(indexedLocale);
+				// console.log(indexedLocale.latitude);
+				if (
+					indexedLocale.latitude < 42 &&
+					indexedLocale.latitude > 27 &&
+					indexedLocale.longitude < 123 &&
+					indexedLocale.longitude > 99 &&
+					indexedLocale.hanzi != null
+				) {
+					// var hanziLength = toString(indexedLocale.hanzi);
 
-		var layerControl = L.control.layers(baseMaps).addTo(map);
-		let outScopeSlider;
-		//This is the bit which initially collects the data from data.json and turns it into markers on the leaflet map, this defines what you see when you first open the main page.
-		fetch('js/data.json')
-			.then((response) => {
-				if (!response.ok) {
-					throw new Error('Network response was not ok');
-				}
-				return response.json();
-			})
-			.then((data) => {
-				// Process the retrieved JSON data
-				// console.log(data);
-				locales = data;
-				// console.log(locales);
-				// Use the retrieved data for further operations (e.g., creating markers on a map)
-				sortIndividualLocalesOnLoad = function (data) {
-					for (let i = 0; i < data.length; i++) {
-						let indexedLocale = data[i];
-						// console.log(indexedLocale);
-						// console.log(indexedLocale.latitude);
-						if (
-							indexedLocale.latitude < 42 &&
-							indexedLocale.latitude > 27 &&
-							indexedLocale.longitude < 123 &&
-							indexedLocale.longitude > 99 &&
-							indexedLocale.hanzi != null
-						) {
-							// var hanziLength = toString(indexedLocale.hanzi);
-
-							// This was a failed attempt to define the labels such that the user only sees Chinese characters, I will attempt to get this bit working at a later date.
-							let chineseCharactersRegex =
-								/[^\u4E00-\u9FFF\s\d!"#$%&'()*+,-./:;<=>?@[\\\]^_`{|}~]/g;
-							let labelChinese = indexedLocale.hanzi;
-							// console.log(labelChinese);
-							labelChinese.replace(chineseCharactersRegex, '');
-							// let chineseOnlyText = indexedLocale.hanzi
-							// 	.match(chineseCharactersRegex)
-							// 	.join('');
-							//Here we can create a condition to skip ahead to the next thing if the entry is filtered out.
-							//This creates the markers and adjusts the size based on the amount of text.
-							markerStyle = L.divIcon({
-								className: 'custom-marker',
-								html: `<div class="marker-text">${indexedLocale.hanzi}</div>`,
-								iconSize: [labelChinese * 1.5, labelChinese * 4],
-							});
-							localeMarker = L.marker(
-								[indexedLocale.latitude, indexedLocale.longitude],
-								{
-									icon: markerStyle,
-								}
-							).addTo(markerGroup);
-							//This defines the popup which you see when you click on a marker.
-							let indexedLocaleString = JSON.stringify(
-								indexedLocale.hanzi +
-									'</p><p>' +
-									indexedLocale.name +
-									'</p><p>' +
-									indexedLocale.polity +
-									'</p><p>' +
-									indexedLocale.location +
-									'</p><p>' +
-									indexedLocale.entries
-							);
-							indexedLocaleString = indexedLocaleString.slice(1, -1);
-							// console.log(indexedLocaleString);
-							localeMarker.bindPopup(indexedLocaleString).openPopup();
-						} else {
-							// console.log('filter this one out');
+					// This was a failed attempt to define the labels such that the user only sees Chinese characters, I will attempt to get this bit working at a later date.
+					let chineseCharactersRegex =
+						/[^\u4E00-\u9FFF\s\d!"#$%&'()*+,-./:;<=>?@[\\\]^_`{|}~]/g;
+					let labelChinese = indexedLocale.hanzi;
+					// console.log(labelChinese);
+					labelChinese.replace(chineseCharactersRegex, '');
+					// let chineseOnlyText = indexedLocale.hanzi
+					// 	.match(chineseCharactersRegex)
+					// 	.join('');
+					//Here we can create a condition to skip ahead to the next thing if the entry is filtered out.
+					//This creates the markers and adjusts the size based on the amount of text.
+					markerStyle = L.divIcon({
+						className: 'custom-marker',
+						html: `<div class="marker-text">${indexedLocale.hanzi}</div>`,
+						iconSize: [labelChinese * 1.5, labelChinese * 4],
+					});
+					localeMarker = L.marker(
+						[indexedLocale.latitude, indexedLocale.longitude],
+						{
+							icon: markerStyle,
 						}
-					}
-				};
-				sortIndividualLocalesOnLoad(locales);
-			});
-		// console.log(outScopeSlider);
-		//This is the same function as earlier but this time filtered with the user defined search term.
+					).addTo(markerGroup);
+					//This defines the popup which you see when you click on a marker.
+					let indexedLocaleString = JSON.stringify(
+						indexedLocale.hanzi +
+							'</p><p>' +
+							indexedLocale.name +
+							'</p><p>' +
+							indexedLocale.polity +
+							'</p><p>' +
+							indexedLocale.location +
+							'</p><p>' +
+							indexedLocale.entries
+					);
+					indexedLocaleString = indexedLocaleString.slice(1, -1);
+					// console.log(indexedLocaleString);
+					localeMarker.bindPopup(indexedLocaleString).openPopup();
+				} else {
+					// console.log('filter this one out');
+				}
+			}
+		};
+		sortIndividualLocalesOnLoad(locales);
+	});
+// console.log(outScopeSlider);
+//This is the same function as earlier but this time filtered with the user defined search term.
 retrieveData = function () {
 	fetch('js/data.json')
 		.then((response) => {
@@ -293,30 +272,32 @@ function clearPolity() {
 	retrieveData();
 }
 function dateFilter() {
+	// Extracted from your code
 	var searchValue = inputResult.value;
 	filterCondition = RegExp(searchValue);
 	console.log(`dates changed to ${rangeHigh}BC - ${rangeLow}BC`);
+
+	// Ensure the correct range values are used here
 	markerGroup.clearLayers();
 	retrieveData();
 }
+
 slidervar.noUiSlider.on('update', function (values, handle) {
 	//handle = 0 if min-slider is moved and handle = 1 if max slider is moved
 	if (handle == 0) {
 		document.getElementById('input-number-min').value = values[0];
 		// console.log(`input no min changed to ${values[0]}`);
 		rangeLow = values[0];
-		dateFilter();
 	} else {
 		document.getElementById('input-number-max').value = values[1];
 		// console.log(`input no max changed to ${values[1]}`);
 		rangeHigh = values[1];
-		dateFilter();
 	}
 	//we will definitely do more here...wait
 });
 searchPolity.addEventListener('click', setPolity);
 clearResult.addEventListener('click', clearPolity);
-retrieveData();
+// retrieveData();
 
 //These are some buttons which I wrote but decided not to include, in future I might re-implement them so I've left the code commented out.
 
